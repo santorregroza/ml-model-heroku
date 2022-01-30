c53101e9111a73c4af0b7fa6dc73ec5172eba755
Santiago <santorregroza@gmail.com>
2022-01-30 13:09:01 -0500

    Fixed sidebar image problem

--- src/page_sidebar.py
+++ src/page_sidebar.py
@@ -4,11 +4,11 @@ import streamlit as st
 def sidebar(ls_page_name):
 
     # Resources
-    image_spainai_url = ""
+    #image_spainai_url = ""
     edition = "Taller despliegue ML"
     title = "### Páginas"
 
-    st.sidebar.image(image_spainai_url)
+    #st.sidebar.image(image_spainai_url)
     st.sidebar.write(edition)
     st.sidebar.write(title)
     page_name = st.sidebar.selectbox("", ls_page_name)
